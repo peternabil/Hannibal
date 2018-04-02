5044aa519d22279c47c4d3192baa59b39a27675f
Peter Nabil <peter@github.com>
2018-04-02 17:53:53 +0200

    @2018

--- out/production/Hannibal/src/cse/Play.java
+++ out/production/Hannibal/src/cse/Play.java
@@ -3,10 +3,7 @@ package cse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-import javax.swing.*;
-import java.awt.event.KeyEvent;
-
-public class Play extends BasicGameState implements KeyListener{
+public class Play extends BasicGameState {
 
     private int x1 = 100;
 
@@ -26,16 +23,26 @@ public class Play extends BasicGameState implements KeyListener{
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         g.setColor(Color.gray);
-        g.fillRect(0,400,800,400-50);
-        for (int j = 0; j < 18; j++) {
-            g.drawImage(new Image("res/col.png"), 10+(j*40), 600);
-            g.drawImage(new Image("res/col.png"),  10+(j*40), 550 - 200);
-        }
-        g.drawImage(new Image("res/New Piskel (2)/Layer 1_sprite_1.png"), x1, 450);
-        g.drawImage(new Image("res/New Piskel (2)/Layer 1_sprite_3.png"),400,450);
+        g.fillRect(0,300,800,400-50);
+//        for (int j = 0; j < 18; j++) {
+//            g.drawImage(new Image("res/col.png"), 10+(j*40), 700);
+//            g.drawImage(new Image("res/col.png"),  10+(j*40), 550 - 200);
+//        }
+        g.drawImage(new Image("res/Column.png"),0,100);
+        g.drawImage(new Image("res/Column.png"),600-10,100);
+        g.drawImage(new Image("res/New Piskel (2)/Layer 1_sprite_1.png"), 200+x1, 450);
+        g.drawImage(new Image("res/New Piskel (2)/Layer 1_sprite_2.png"),300,450);
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+        Input input = gc.getInput();
+        if (input.isControllerRight(Input.KEY_RIGHT))
+            x1-=1;
+        if (input.isControllerLeft(Input.KEY_LEFT))
+            x1+=1;
     }
 }
+
+
+/// @2018
