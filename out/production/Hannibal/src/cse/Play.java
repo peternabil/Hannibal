b85cbbbda07d1baa4b2e6d7fd875302d09ea3430
Peter Nabil <peter@github.com>
2018-04-09 20:13:57 +0200

    a new update on the character and the first animation

--- out/production/Hannibal/src/cse/Play.java
+++ out/production/Hannibal/src/cse/Play.java
@@ -6,10 +6,10 @@ import org.newdawn.slick.state.*;
 public class Play extends BasicGameState {
 
     private int x1 = 100;
-    Animation roman,carthaginian;
+    Animation roman,roman2,carthaginian;
     boolean ex = false;
     float x = 0 , y = 0;
-    int [] waitingtime = {300,300};
+    int [] waitingtime = {300};
 
     public Play(int state){
 
@@ -21,32 +21,30 @@ public class Play extends BasicGameState {
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        Image [] romansoldier = {new Image("res/New Piskel (3)/Layer 1_sprite_1.png")};
+        roman = new Animation(romansoldier,waitingtime,false);
+        Image [] romansoldier2 = {new Image("res/New Piskel (2)/Layer 1_sprite_1.png")};
+        roman2 = new Animation(romansoldier2,waitingtime,false);
+        Image [] cartigiansoldier = {new Image("res/New Piskel (2)/Layer 1_sprite_3.png")};
+        carthaginian = new Animation(cartigiansoldier,waitingtime,false);
 
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         g.setColor(Color.gray);
-        g.fillRect(0,300,800,400-50);
-//        for (int j = 0; j < 18; j++) {
-//            g.drawImage(new Image("res/col.png"), 10+(j*40), 700);
-//            g.drawImage(new Image("res/col.png"),  10+(j*40), 550 - 200);
-//        }
-        g.drawImage(new Image("res/Column.png"),0,100);
-        g.drawImage(new Image("res/Column.png"),600-10,100);
-        g.drawImage(new Image("res/New Piskel (2)/Layer 1_sprite_1.png"), 200+x, 450);
-        g.drawImage(new Image("res/New Piskel (2)/Layer 1_sprite_2.png"),300,450);
-//        g.“SpriteSheet(Image image, int tw, int th);
+        g.fillRect(0,600+15,800,799-450);
+        roman.draw(200+x, 450+y);
+        g.drawImage(new Image("res/Column.png"),-10,50);
+        g.drawImage(new Image("res/Column.png"),600-15,50);
     }
-
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
-
         Input input = gc.getInput();
         if (input.isKeyDown(Input.KEY_RIGHT))
-            x+=1*i;
-        else if (input.isControllerLeft(Input.KEY_LEFT))
-            x-=1*i;
+            x+=i*.1f;
+        if (input.isKeyDown(Input.KEY_LEFT))
+            x-=i*.1f;
     }
 }
 
